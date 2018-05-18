0cdafd0bf378091dda129c7a6224212a2c6d05df
Leo Ertuna <leo.ertuna@gmail.com>
2018-05-19 00:07:00 +0300

    Helper to build a list of destination files

---
+++ src/main/java/io/github/jpleorx/jcopy/helpers/DestinationFilesHelper.java
@@ -0,0 +1,88 @@
+package io.github.jpleorx.jcopy.helpers;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * This is a small helper that will build a list of destination files
+ *
+ * @author Leo Ertuna
+ * @since 19.05.2018 00:02
+ */
+public class DestinationFilesHelper {
+    private File destinationFolder;
+    private List<String> filenames;
+    private String sourceFileExtension;
+    private List<File> destinationFiles;
+
+    /**
+     * Constructor
+     * @param destinationFolder the destination folder
+     * @param filenames list of filenames
+     * @param sourceFileExtension extension of the source file
+     */
+    public DestinationFilesHelper(File destinationFolder, List<String> filenames, String sourceFileExtension) {
+        this.destinationFolder = destinationFolder;
+        this.filenames = filenames;
+        this.sourceFileExtension = sourceFileExtension;
+        this.destinationFiles = new CopyOnWriteArrayList<>(new ArrayList<>(filenames.size()));
+    }
+
+    /**
+     * Internal building procedure
+     */
+    private void buildInternal() {
+        // For each filename in a parallel stream
+        filenames.parallelStream().forEach(name -> {
+            // Get the folder path
+            String folderPath = destinationFolder.getAbsolutePath();
+
+            // Add file name and extension to the folder path - we will get filepath
+            String filePath = folderPath + "/" + name + "." + sourceFileExtension;
+
+            // Create new file and add it to the list
+            destinationFiles.add(new File(filePath));
+        });
+    }
+
+    /**
+     * External control method for building procedure
+     */
+    public void build() {
+        this.buildInternal();
+    }
+
+    /**
+     * Getter
+     * @return destination folder
+     */
+    public File getDestinationFolder() {
+        return destinationFolder;
+    }
+
+    /**
+     * Getter
+     * @return list of filenames
+     */
+    public List<String> getFilenames() {
+        return filenames;
+    }
+
+    /**
+     * Getter
+     * @return extension of the source file
+     */
+    public String getSourceFileExtension() {
+        return sourceFileExtension;
+    }
+
+    /**
+     * Getter
+     * @return list of destination files
+     */
+    public List<File> getDestinationFiles() {
+        return destinationFiles;
+    }
+}
